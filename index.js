567726d1f7ab704e8f97ffa54f754df1f9149f84
Shubham Verma <sverma3045@gmail.com>
2022-05-10 14:46:01 +0530

    backend

---
+++ index.js
@@ -0,0 +1,33 @@
+// Import Express
+const express = require('express')
+const mongoose = require('mongoose')
+const apiroute = require('./routes/api_route')
+const cors = require('cors')
+const dotenv = require('dotenv')
+// Initialize the application
+const app = express()
+
+// setup server port
+const port = 3005
+app.use(express.json(),cors())
+
+
+// send message for your localhost
+app.use('/api/users', apiroute)
+
+dotenv.config()
+
+app.get('/', (req, res) => {
+    res.send("App running")
+})
+
+mongoose.connect(process.env.DB_CONNECT, {useNewUrlParser:true} )
+.then(() =>{
+    console.log('Database Connected')
+}).catch(err => console.log(err))
+
+
+// Launch the ecommerce backend app
+app.listen(port, () =>{
+    console.log(`running app on port: 'http://localhost:${port}/`)
+})
